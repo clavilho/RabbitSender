17ac884796ebecb8f452b7da55ec3640cadfbb56
João Vitor Clavilho <jv.clavilho@hotmail.com>
2024-08-31 10:58:10 -0300

    Inicial commit

---
+++ PublicadorDeMensagem/PublicadorDeMensagem/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace PublicadorDeMensagem;
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.Write("Digite a mensagem que deseja enviar para a fila: ");
+        var mensagemParaEnviar = Console.ReadLine();
+        PublicarMensagemNoRabbit(mensagemParaEnviar);
+    }
+
+    public static void PublicarMensagemNoRabbit(string mensagemParaEnviar)
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "queue1",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+
+        string mensagem = mensagemParaEnviar;
+        var body = Encoding.UTF8.GetBytes(mensagem);
+
+        channel.BasicPublish(exchange: string.Empty, routingKey: "queue1", basicProperties: null, body: body);
+
+        Console.WriteLine($"[X] Enviado {mensagem}");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # RabbitSender
+
+## Descrição
+
+O **RabbitSender** é uma aplicação de console em .NET projetada por mim para enviar mensagen para uma fila do RabbitMQ. É uma ferramenta simples e eficiente para desenvolvedores testarem e integrarem o RabbitMQ em seus sistemas.
+
+## Pré-requisitos
+
+Antes de começar, você precisará ter instalado:
+
+- [.NET SDK](https://dotnet.microsoft.com/download) (versão mínima recomendada: 6.0)
+- [Docker](https://www.docker.com/products/docker-desktop) (opcional, para executar o RabbitMQ em um contêiner)
+- Um servidor RabbitMQ em execução
+
+## Configuração do RabbitMQ
+
+Se você ainda não tiver um servidor RabbitMQ em execução, você pode facilmente configurá-lo usando Docker:
+
+```bash
+docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
